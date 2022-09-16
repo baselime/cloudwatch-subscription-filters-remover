cbd146a94e918cbfe4a3d47dcf31c0dccab7de71
Boris Tane <boris.tane@gmail.com>
2022-09-16 09:06:58 +0100

    implements lambda function to delete subscription filters

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # cloudwatch-subscription-filters-remover
-A lambda function to loop through your CloudWatch logs groups and remove the subscription filters matching a criteria
+A service to loop through your CloudWatch logs groups and remove the subscription filters matching a criteria
+
+---
+
+## Architecture
+
+The service comprises:
+- A Lambda function: `cloudwatch-sub-filters-remover-prod-command`
+- An SQS queue: `cloudwatch-subscription-filters-remover-queue`
+- A dead-letter queue: `cloudwatch-subscription-filters-remover-dead-letter-queue`
+
+The Lambda function is triggered when a message is sent to the SQS queue. The Lambda function loops through all the log groups in the provided AWS region and checks for subscription filters matching the criteria set in the message sent to the SQS queue. When a subscription filter is found, it is deleted.
+
+## Installation
+
+This lambda function has no other deps than the AWS SDK which is provided in the Lambda runtime by default.
+
+## Deployment
+
+The service is deployed with the [Serverless Framework](https://serverless.com). Please make sure you have the serverless CLI installed locally or if using a CI pipeline, it must be installed in your CI environment.
+
+Before deployment, ensure you've edited the region in the `serverless.yml` file. The default region is `eu-west-2`.
+
+Run the following command to deploy:
+
+``` bash
+sls deploy --stage <stage>
+```
+
+where `<stage>` is the stage you want to deploy the service to; typically `prod`/`dev`/`uat`.
+
+## Invocation
+
+To invoke the service, you must send a message to the SQS queue. The form of the message must be:
+
+```
+{"prefix": "<prefix>"}
+```
+
+where `<prefix>` is the prefix of the subscription filters you want to delete.
+
+You can send those messages to the queue using the [AWS Console](https://aws.amazon.com) or with the AWS CLI:
+
+```bash
+aws sqs send-message --queue-url https://sqs.<region>.amazonaws.com/<account-id>/cloudwatch-subscription-filters-remover-dead-letter-queue --message-body '{"prefix": "<prefix>"}'
+```
+
+where:
+- `<region>` is the AWS region
+- `<account-id>` is your AWS account id
+- `<prefix>` is the prefix of the subscriptions filters to delete
+ 
---
+++ src/main.js
@@ -0,0 +1,84 @@
+const AWS = require("aws-sdk");
+
+const logs = new AWS.CloudWatchLogs({ region: process.env.AWS_REGION });
+
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+async function listLogGroups() {
+
+  let groups = [];
+  let marker = null;
+  do {
+    const { logGroups, nextToken } = (await logs.describeLogGroups({ nextToken: marker }).promise());
+    if (logGroups) {
+      groups = [...groups, ...logGroups];
+    }
+    marker = nextToken;
+  } while (marker);
+  return groups;
+}
+
+
+async function command(event) {
+  const { Records } = event;
+
+  const [record,] = Records;
+
+  let message = JSON.parse(record.body || "");
+  if (message.Message) { message = JSON.parse(message.Message); }
+
+  if (!message.prefix) {
+    throw new Error(`Missing subscription prefix in the command message: ${JSON.stringify(message)}`);
+  }
+
+  logger.info("Deleting the log group subscription filters", message);
+  const groups = await listLogGroups();
+
+  logger.info("All the log groups in the region", { groups: groups.map(fn => fn.logGroupName) });
+  if (!groups || groups.length === 0) {
+    return;
+  }
+
+  let count = 0;
+
+  const promises = groups.map(async group => {
+    logger.info("Processing log group", { group: group.logGroupName });
+    const { logGroupName } = group;
+
+    const subscriptions = (await logs.describeSubscriptionFilters({ logGroupName, }).promise()).subscriptionFilters;
+    logger.info("The subscription filters found for this log group", { logGroupName, subscriptions, });
+    if (!subscriptions || subscriptions.length === 0) return;
+    const subscription = subscriptions.find(s => s.filterName.startsWith(message.prefix));
+    if (!subscription) return;
+
+    try {
+      const params = {
+        logGroupName,
+        filterName: subscription.filterName
+      };
+      count += 1;
+      await delay(count * 10);
+      await logs.deleteSubscriptionFilter(params).promise();
+      logger.info("Deleted the subscription filter", { params });
+      return logGroupName;
+    } catch (error) {
+      logger.error("There was an error deleting the log subscription filter", { error, params });
+    }
+  });
+
+  const res = await Promise.all(promises);
+  logger.info("Deleted the log subscription filters", { res: res.filter(r => r), count });
+}
+
+
+const logger = {
+  info: (message, data) => { console.info(JSON.stringify({ message, data })) },
+  error: (message, data) => { console.error(JSON.stringify({ message, data })) },
+}
+
+module.exports = {
+  command,
+}
